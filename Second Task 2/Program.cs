329936496e30c091607c2f1b888fc016f8c21e12
Heshamsayed103 <hesham58382004@gmail.com>
2025-08-08 15:43:23 +0300

    Second Task

---
+++ Second Task 2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Second_Task_2
+{
+
+    public class MultiBox<T>
+    {
+        private List<T> items = new List<T>();
+
+        public void InsertItem(T item)
+        {
+            items.Add(item);
+        }
+
+        public List<T> GetAllItems()
+        {
+            return items;
+        }
+
+        public int Count()
+        {
+            return items.Count;
+        }
+
+        public bool IsEmpty()
+        {
+            return items.Count == 0;
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            MultiBox<string> stringBox = new MultiBox<string>();
+
+            
+            Console.WriteLine(stringBox.IsEmpty()); // Output: True
+
+            
+            stringBox.InsertItem("Apple");
+            stringBox.InsertItem("Banana");
+
+            
+            Console.WriteLine(stringBox.Count()); // Output: 2
+        }
+    }
+
+}
+
